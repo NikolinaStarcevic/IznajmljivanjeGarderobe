2a586df1517679e0e71c7c88e58514640c825204
NikolinaStarcevic <nikolinastarcevic@tehnickacacak.edu.rs>
2025-06-24 23:34:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -22,16 +22,29 @@ document.getElementById("rentalForm").addEventListener("submit", function (e) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!datum || izabraniDatum < danas) {
-        alert("Molimo unesite ispravan datum iznajmljivanja (ne mo~e biti u proalosti).");
+        alert("Molimo unesite ispravan datum iznajmljivanja (ne može biti u prošlosti).");
         return;
     }
     if (!emailRegex.test(email)) {
         alert("Molimo unesite validnu email adresu.");
         return;
     }
-    window.alert("Uspeano ste iznajmili:"+haljina+ "\nza datum " +datum+"\nPotvrda e biti poslata na "+email+".");
+    window.alert("Uspešno ste iznajmili:"+haljina+ "\nza datum " +datum+"\nPotvrda će biti poslata na "+email+".");
     this.reset();
     bootstrap.Modal.getInstance(document.getElementById("rentalModal")).hide();
 });
+document.getElementById("rentalForm").addEventListener("submit", function (e) {
+    e.preventDefault();
 
+    const ime = document.getElementById("iznajmiIme").value;
+    const email = document.getElementById("iznajmiEmail").value;
+    const datum = document.getElementById("iznajmiDatum").value;
+    const placanje = document.getElementById("nacinPlacanja").value;
+    const vracanje = document.getElementById("vracanje").value;
+    const haljina = document.getElementById("dressName").value;
 
+    alert(`Hvala, ${ime}!\n\nIznajmili ste: ${haljina}\nDatum: ${datum}\nPlaćanje: ${placanje}\nVraćanje: ${vracanje}`);
+    this.reset();
+    const modal = bootstrap.Modal.getInstance(document.getElementById('rentalModal'));
+    modal.hide();
+});
